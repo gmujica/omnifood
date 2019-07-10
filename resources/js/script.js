b126a141db11d15eb94e0d1a8c5ebdde6bf668b1
gmujica <gregorymujica1993@gmail.com>
2019-07-10 16:31:49 -0500

    sticky nav with jquery DONE

--- resources/js/script.js
+++ resources/js/script.js
@@ -1,3 +1,24 @@
 $(document).ready(function() {
-    
+
+    $('.js--section-features').waypoint(function(direction) {
+        if (direction == "down") {
+            $('nav').addClass('sticky');
+        }else {
+            $('nav').removeClass('sticky');
+        }
+    },
+    {
+       offset: '60px'
+    });
+
+    /* argumento direction permite identificar si el scroll va hacia abajo o hacia arriba */
+    /*
+    var waypoints = $('#handler-first').waypoint(function(direction) {
+    notify(this.element.id + ' hit 25% from top of window') 
+    },
+     {
+        offset: '25%'
+    })
+    */
+
 })
